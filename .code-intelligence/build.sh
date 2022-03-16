dc9ddc3a19558ecdd97b57e995882305ead07325
wojtek <dziewiecki@code-intelligence.com>
2022-03-16 17:01:53 +0100

    build script that allows faster fuzz target compilation

--- .code-intelligence/build.sh
+++ .code-intelligence/build.sh
@@ -1,5 +1,10 @@
 set -e
-./autogen.sh
-./configure
-make -j$(nproc)
+BUILDPROJECT=0
+if test $BUILDPROJECT = 1
+then
+    ./autogen.sh
+    ./configure
+    make clean
+    make -j$(nproc)
+fi
 # Note: The build script will be executed from the project's root directory.
