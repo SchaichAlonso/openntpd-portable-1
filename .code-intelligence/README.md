bb5626ec6d1fa6e8dde44a289e06cdef2e178532
wojtek <dziewiecki@code-intelligence.com>
2022-03-25 11:06:23 +0100

    example library fuzz test, better fuzzing readme

--- .code-intelligence/README.md
+++ .code-intelligence/README.md
@@ -1,2 +1,4 @@
+please start ci-daemon --copy_policy=copy for this project to work
+
 After creating fuzzing project, you have to run ./autogen.sh on the host to get all source code,
 if you want to see it and write API fuzz targets.
--- .code-intelligence/fuzz_targets/useless_fuzz_target.cpp
+++ .code-intelligence/fuzz_targets/useless_fuzz_target.cpp
@@ -1,8 +1,10 @@
 #include <stdint.h>
 #include <stddef.h>
+#include <fuzzer/FuzzedDataProvider.h>
 
 extern "C" {
-#include <sys/types.h>
+//#include "unistd.h"
+/* #include <sys/types.h>
 #include <sys/time.h>
 #include <sys/stat.h>
 #include <errno.h>
@@ -10,17 +12,17 @@ extern "C" {
 #include <paths.h>
 #include <poll.h>
 #include <pwd.h>
-#include <signal.h>
+// #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
 #include <syslog.h>
 #include <time.h>
-#include <unistd.h>
+// #include <unistd.h>
 #include <err.h>
-#include "ntpd.h"
+#include "ntpd.h" */
 }
 
-void ntp_sighdlr(int sig);
+extern "C" void ntp_sighdlr(int sig);
 
 // extern "C" int FUZZ_INIT_WITH_ARGS(int *argc, char ***argv) {
 extern "C" int FUZZ_INIT() {
@@ -30,11 +32,11 @@ extern "C" int FUZZ_INIT() {
   return 0;
 }
 
-extern "C" int FUZZ(const int *Data, size_t Size) {
-
+extern "C" int FUZZ(const uint8_t *Data, size_t Size) {
+  FuzzedDataProvider dp(Data, Size);
   // process fuzzer input (*Data) and setup local objects necessary to call the function under test
-
-  ntp_sighdlr(Data[0]);
+  int input = dp.ConsumeIntegral <int> (); 
+  ntp_sighdlr(input);
 
   // reset state and free all locally allocated resources
 
