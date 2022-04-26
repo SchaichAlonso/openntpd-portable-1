70524d4d4ea6b4c3de5a78ca9c47c93ba77b1057
0roman <roman.wagner18@gmx.de>
2022-04-26 10:43:26 +0200

    change TCP to UDP fuzz target

--- .code-intelligence/fuzz_targets/udp_fuzzer.cpp
+++ .code-intelligence/fuzz_targets/udp_fuzzer.cpp
@@ -1,12 +1,13 @@
 
 #include <flibc.h>
 #include <unistd.h>
- 
+#include <stdio.h>
+
 #include "ntp.h"
  
 extern "C" uint8_t processing_input;
  
-FUZZ_TCP_SERVER(original_main, 123, int client_fd, const uint8_t* data, size_t size) {
+FUZZ_UDP_SERVER(original_main, 123, int client_fd, const uint8_t* data, size_t size) {
  
  // TODO: ensure to sent max NTP_MSGSIZE bytes
     if (size < NTP_MSGSIZE) {
