eb060fe8cf9190446757b01ad8cb9a585be97f9d
0roman <roman.wagner18@gmx.de>
2023-12-19 10:15:08 +0100

    add ability to generate lcov report with cifuzz Make target is added that enables generating coverage reports for the fuzztarget. main_hook respects now passing cifuzz and libfuzzer args without conflicting with ntpd args

--- libfuzzer_no_main/main_hook.cpp
+++ libfuzzer_no_main/main_hook.cpp
@@ -1,5 +1,7 @@
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
+#include <string>
 
 #include <chrono>
 #include <iostream>
@@ -15,8 +17,41 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size);
 extern "C" int LLVMFuzzerRunDriver(int *argc, char ***argv,
                   int (*UserCb)(const uint8_t *Data, size_t Size));
 
+// remove arg that start with x from argv
+void remove_arg(int &argc, char **argv, const char* x) {
+  std::string str;
+  int len = std::strlen(x);
+
+  for (int i = 1; i < argc; i++) {
+    str = std::string (argv [i]);
+    if (str.rfind(x, 0) == 0) {
+      for (int j = i; j < argc; j++) {
+        argv [j] = argv [j + 1];
+      }
+      argc--;
+      break;
+    }
+  }
+}
 
 int our_main(int argc, char** argv, char** envp) {
+    // cifuzz or libfuzzer check for arguments in argv that conflict with
+    // args that ntpd expects via argv. Args that are set from cifuzz or
+    // for libfuzzer will be saved in orig_argc and orig_argv
+    int orig_argc = argc;
+    char** orig_argv = new char*[argc];
+    for (int i = 0; i < argc; i++) {
+        orig_argv[i] = new char[strlen(argv[i]) + 1];
+        strcpy(orig_argv[i], argv[i]);
+    }
+
+    remove_arg(argc, argv, "-max_total_time=");
+    remove_arg(argc, argv, "/openntpd-portable/.cifuzz-corpus");
+    remove_arg(argc, argv, "-artifact_prefix=");
+    remove_arg(argc, argv, "-merge=");
+    remove_arg(argc, argv, "/tmp/llvm-coverage");
+    remove_arg(argc, argv, "-runs=");
+    // remove_arg(argc, argv, "");
 
     if(getenv("FUZZER_RUNNING")) {
 
@@ -33,7 +68,7 @@ int our_main(int argc, char** argv, char** envp) {
         //std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 
         std::cout << "[premain] Starting fuzzer..." << std::endl;
-        return LLVMFuzzerRunDriver(&argc, &argv, LLVMFuzzerTestOneInput);
+        return LLVMFuzzerRunDriver(&orig_argc, &orig_argv, LLVMFuzzerTestOneInput);
     }
     
 }
