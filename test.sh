7df780fa3b09bf8dba504f40c05c0cf6014ab9c2
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-05 16:46:57 +0000

    Add script to collect coverage manually

---
+++ test.sh
@@ -0,0 +1,11 @@
+find /tmp -name '*.profraw' -exec unlink {} \;
+find /tmp -name '*.profraw' -exec unlink {} \;
+find /tmp -name '*.profdata' -exec unlink {} \;
+
+export ASAN_OPTIONS=detect_leaks=0
+export NO_CIFUZZ=1
+export LLVM_PROFILE_FILE='/tmp/ntpd_fuzzer_coverage.%p.profraw'
+
+/openntpd-portable/ntpd_fuzzer_coverage -runs=10000
+llvm-profdata merge -sparse /tmp/*.profraw -o /tmp/ntp_fuzzer_coverage.profdata
+llvm-cov report ./ntpd_fuzzer_coverage -instr-profile=/tmp/ntp_fuzzer_coverage.profdata
