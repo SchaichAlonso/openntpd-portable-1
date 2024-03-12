ffd60ebacb97a1f4b5137bdc2a8f0893b106b6c8
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-12 10:23:58 +0000

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,12 @@
+find /tmp -name 'ntpd.*.pid' -exec rm -v {} \;
+find /tmp -name '*.profraw' -exec rm -v {} \;
+find /tmp -name '*.profdata' -exec rm -v {} \;
+
+export ASAN_OPTIONS=detect_leaks=0
+export NO_CIFUZZ=1
+export LLVM_PROFILE_FILE='/tmp/ntpd_fuzzer_coverage.%p.profraw'
+FUZZ_TARGET=/openntpd-portable/ntpd_fuzzer_coverage_internal
+
+${FUZZ_TARGET} -runs=1000
+llvm-profdata merge -sparse /tmp/*.profraw -o /tmp/ntp_fuzzer_coverage.profdata
+llvm-cov report ${FUZZ_TARGET} -instr-profile=/tmp/ntp_fuzzer_coverage.profdata
