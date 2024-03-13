8058c25157b3f8f3472454d2fce1b16c6743a4cd
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-13 20:17:36 +0900

    Increase iteration count

--- test.sh
+++ test.sh
@@ -9,6 +9,6 @@ export ASAN_OPTIONS=detect_leaks=0
 export NO_CIFUZZ=1
 export LLVM_PROFILE_FILE=/tmp/${FUZZ_TARGET}.%p.profraw
 
-${FUZZ_TARGET_PATH} -runs=1000
+${FUZZ_TARGET_PATH} -runs=10000000
 llvm-profdata merge -sparse /tmp/${FUZZ_TARGET}.*.profraw -o /tmp/${FUZZ_TARGET}.profdata
 llvm-cov report ${FUZZ_TARGET} -instr-profile=/tmp/${FUZZ_TARGET}.profdata
