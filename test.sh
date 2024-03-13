b627d391598064362b1520f133e9b11bfa1eb701
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-13 18:03:30 +0900

    Sync with cifuzz-new

--- test.sh
+++ test.sh
@@ -2,10 +2,11 @@ find /tmp -name 'ntpd.*.pid' -exec rm -v {} \;
 find /tmp -name '*.profraw' -exec rm -v {} \;
 find /tmp -name '*.profdata' -exec rm -v {} \;
 
+FUZZ_TARGET=/openntpd-portable/ntpd_fuzzer_coverage
 export ASAN_OPTIONS=detect_leaks=0
 export NO_CIFUZZ=1
-export LLVM_PROFILE_FILE='/tmp/ntpd_fuzzer_coverage.%p.profraw'
+export LLVM_PROFILE_FILE='/tmp/${FUZZ_TARGET}.%p.profraw'
 
-/openntpd-portable/ntpd_fuzzer_coverage -runs=10000
-llvm-profdata merge -sparse /tmp/*.profraw -o /tmp/ntp_fuzzer_coverage.profdata
-llvm-cov report ./ntpd_fuzzer_coverage -instr-profile=/tmp/ntp_fuzzer_coverage.profdata
+${FUZZ_TARGET} -runs=1000
+llvm-profdata merge -sparse /tmp/${FUZZTARGET}.*.profraw -o /tmp/ntp_fuzzer_coverage.profdata
+llvm-cov report ${FUZZ_TARGET} -instr-profile=/tmp/ntp_fuzzer_coverage.profdata
