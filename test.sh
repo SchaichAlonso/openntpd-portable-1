d2d3fafaf6ab6e1f11f0ed7485ab89a984df0bdf
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-13 18:07:55 +0900

    Name the profiling data

--- test.sh
+++ test.sh
@@ -1,12 +1,14 @@
 find /tmp -name 'ntpd.*.pid' -exec rm -v {} \;
-find /tmp -name '*.profraw' -exec rm -v {} \;
-find /tmp -name '*.profdata' -exec rm -v {} \;
+find /tmp -name "${FUZZ_TARGET}.*.profraw" -exec rm -v {} \;
+find /tmp -name "${FUZZ_TARGET}.profdata" -exec rm -v {} \;
 
-FUZZ_TARGET=/openntpd-portable/ntpd_fuzzer_coverage
+FUZZ_TARGET=ntpd_fuzzer_coverage
+FUZZ_TARGET_PATH=/openntpd-portable/ntpd_fuzzer_coverage
+
 export ASAN_OPTIONS=detect_leaks=0
 export NO_CIFUZZ=1
 export LLVM_PROFILE_FILE='/tmp/${FUZZ_TARGET}.%p.profraw'
 
-${FUZZ_TARGET} -runs=1000
-llvm-profdata merge -sparse /tmp/${FUZZTARGET}.*.profraw -o /tmp/ntp_fuzzer_coverage.profdata
-llvm-cov report ${FUZZ_TARGET} -instr-profile=/tmp/ntp_fuzzer_coverage.profdata
+${FUZZ_TARGET_PATH} -runs=1000
+llvm-profdata merge -sparse /tmp/${FUZZTARGET}.*.profraw -o /tmp/${FUZZTARGET}.profdata
+llvm-cov report ${FUZZ_TARGET} -instr-profile=/tmp/${FUZZTARGET}.profdata
