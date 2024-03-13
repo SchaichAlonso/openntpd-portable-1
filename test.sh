c75bdc8d64753d6ff1f674cdc5874f5b9620e515
Alonso Schaich <alonsoschaich@fastmail.fm>
2024-03-14 00:51:04 +0900

    We're in the current path anyways

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 FUZZ_TARGET=ntpd_fuzzer_coverage
-FUZZ_TARGET_PATH=/openntpd-portable/ntpd_fuzzer_coverage
+FUZZ_TARGET_PATH=./ntpd_fuzzer_coverage
 
 find /tmp -name 'ntpd.*.pid' -exec rm -v {} \;
 find /tmp -name "${FUZZ_TARGET}.*.profraw" -exec rm -v {} \;
@@ -12,4 +12,4 @@ export LLVM_PROFILE_FILE=/tmp/${FUZZ_TARGET}.%p.profraw
 
 ${FUZZ_TARGET_PATH} -runs=10000000
 llvm-profdata merge -sparse /tmp/${FUZZ_TARGET}.*.profraw -o /tmp/${FUZZ_TARGET}.profdata
-llvm-cov report ${FUZZ_TARGET} -instr-profile=/tmp/${FUZZ_TARGET}.profdata
+llvm-cov report ${FUZZ_TARGET_PATH} -instr-profile=/tmp/${FUZZ_TARGET}.profdata
